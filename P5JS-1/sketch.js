6ea804e2ca1e0659a57657776b7b55c06f880937
Blake <rblakehampton@gmail.com>
2021-03-10 10:59:36 -0700

    Lines

---
+++ P5JS-1/sketch.js
@@ -0,0 +1,21 @@
+function setup(){
+    createCanvas(400,400);
+    var length = 400;
+    var hight = 400;
+
+}
+function draw(){
+    Lines()
+}
+
+function Lines(){
+    stroke(0)
+    // line(0,0,400,400)
+    line(0,400,200,0)
+    delay (1)
+    line(200,0,400,400)
+
+}
+
+
+  
